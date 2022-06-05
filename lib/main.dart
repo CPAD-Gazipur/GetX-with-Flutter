d4b1b009dd80f0f274e9e4fb96bf6a8aff6e9bc0
karno786 <amin15-1951@diu.edu.bd>
2022-06-05 17:35:32 +0600

    GetX Flutter - Web Scrolling Added !!

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:getx_with_flutter/getx_example/dummy_api_app/dummy_api.dart';
 import 'package:getx_with_flutter/getx_example/obx_example.dart';
 import 'package:getx_with_flutter/getx_example/todo_example.dart';
 import 'package:getx_with_flutter/getx_utils/getx_utils.dart';
+import 'package:getx_with_flutter/utils/app_scroll_behaviour.dart';
 import 'package:getx_with_flutter/utils/localization_page.dart';
 import 'ui/home_screen.dart';
 
@@ -17,6 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      scrollBehavior: AppScrollBehavior(),
       locale: LocalizationService.locale,
       fallbackLocale: LocalizationService.fallBackLocale,
       translations: LocalizationService(),
